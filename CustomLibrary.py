615e983fe9fbca7c406028958d3219e80e11f37f
Angeline Rohan <angeline.rohan@kcc.com>
2025-02-07 09:46:31 -0500

    File_Cleanup

--- CustomLibrary.py
+++
@@ -1,7 +0,0 @@
-from selenium import webdriver
-
-class CustomLibrary:
-    def get_console_logs(self, log_type='browser'):
-        driver = webdriver.Chrome()
-        logs = driver.get_log(log_type)
-        return logs
